dfd0fcb7d978eb03baa783f05ecb382ad1b80892
GauravShreenet <gthakuri656@gmail.com>
2024-06-20 15:53:23 +1000

    dropdown menu closes when clicked elsewhere

--- src/component/layout/NavBar.jsx
+++ src/component/layout/NavBar.jsx
@@ -30,6 +30,8 @@ export const NavBar = () => {
   const debounceSearch = useDebounce(searchTerm, 500)
   const [isOpen, setIsOpen] = useState(false)
 
+  const dropdownRef = useRef(null);
+
   useEffect(() => {
     if (debounceSearch.length > 1) {
       const searchQuery = debounceSearch.toLowerCase()
@@ -43,6 +45,24 @@ export const NavBar = () => {
     setIsDropdownOpen(!isDropdownOpen);
     setColorChange(true)
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsDropdownOpen(false);
+      }
+    };
+  
+    if (isDropdownOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+  
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isDropdownOpen]);
 
   const changeColor = () => {
     if (window.scrollY >= 80) {
@@ -103,7 +123,7 @@ export const NavBar = () => {
             <Hamburger toggled={isOpen} toggle={setIsOpen} />
           </div>
           <div className='relative'>
-            <ul className={`flex p-4 md:p-0 md:relative font-medium gap-5 md:flex-row ${isOpen ? 'flex-col justify-center absolute  w-[100vh] md:w-full bg-gray-900 md:bg-transparent md:mt-4' : 'hidden md:flex'}`}>
+            <ul className={`flex p-4 md:p-0 md:relative font-medium gap-5 md:flex-row ${isOpen ? 'flex-col justify-center absolute w-[100vh] md:w-full bg-gray-900 md:bg-transparent md:mt-4' : 'hidden md:flex'}`}>
               <li>
                 <Link to="/" className="block py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</Link>
               </li>
@@ -168,7 +188,7 @@ export const NavBar = () => {
                   </button>
 
                   {isDropdownOpen && (
-                    <div className="absolute -right-1 z-10 mt-6 w-56 origin-top-right rounded-b-md bg-gray-900 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
+                    <div className="absolute -right-1 z-10 mt-6 w-56 origin-top-right rounded-b-md bg-gray-900 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1" ref={dropdownRef}>
                       <div className="" role="none">
                         <Link to="/edit-profile" className="text-white block px-4 py-2 text-sm hover:bg-gray-600" role="menuitem" tabIndex="-1" id="menu-item-0">Account settings</Link>
                         <Link to="/orders" className="text-white block px-4 py-2 text-sm hover:bg-gray-600" role="menuitem" tabIndex="-1" id="menu-item-2">Orders</Link>
